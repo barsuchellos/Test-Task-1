a0d9248e16a91f87bb451963d4d721b9417e4002
Bohdan Borsuk <barsuchellos@gmail.com>
2023-10-14 22:02:15 +0300

    fixed logic in the Question component

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -3,21 +3,19 @@ import icon from '../../images/icons/add.png'
 import styles from './styles.module.scss'
 
 const Question = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [iconRotation, setIconRotation] = useState(0);
-    
+    const [isOpen, setIsOpen] = useState({ qusetionVisible: false, iconRotation: 0 })
+
     const toggleExpand = () => {
-        setIsOpen(!isOpen)
-        setIconRotation(iconRotation + 180)
+        setIsOpen(prev => ({ qusetionVisible: !prev.qusetionVisible, iconRotation: prev.iconRotation + 180}))
     }
 
     return (
         <div className={styles.container}>
-            <div className={`${styles.container_question} ${isOpen ? styles.clicked : ''}`} onClick={toggleExpand}>
+            <div className={`${styles.container_question} ${isOpen.qusetionVisible ? styles.clicked : ''}`} onClick={toggleExpand}>
                 <p className={styles.container_title}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                <img src={icon} alt="" className={`${styles.containerIcon} ${styles.rotate}`} style={{ transform: `rotate(${iconRotation}deg)` }} />
+                <img src={icon} alt="" className={`${styles.containerIcon} ${styles.rotate}`} style={{ transform: `rotate(${isOpen.iconRotation}deg)` }} />
             </div>
-            {isOpen &&
+            {isOpen.qusetionVisible &&
                 <p className={styles.container_answer}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
